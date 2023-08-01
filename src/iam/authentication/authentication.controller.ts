0332ccf8287ddccf07c7a55c3baa14168d0128d6
unstoppab1e <unstoppab1e@users.noreply.github.com>
2023-08-02 04:26:41 +0900

    refresh tokens

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -4,6 +4,7 @@ import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto/sign-in.dto';
 import { AuthType } from './enums/auth-type.enums';
 import { Auth } from './decorators/auth.decorator';
+import { RefreshTokenDto } from './dto/refresh-token.dto/refresh-token.dto';
 
 @Auth(AuthType.None)
 @Controller('authentication')
@@ -19,5 +20,11 @@ export class AuthenticationController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('refresh-tokens')
+  refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto);
   }
 }
